9309962b6a7f9722f3a9db3b4f25b09a8c7ca74c
nayaksomkar <nayaksomkar+github.dev@outlook.in>
2024-01-31 18:47:09 +0530

    modified:   DSA/JavaScript/fizzBuzz.js

--- DSA/JavaScript/fizzBuzz.js
+++ DSA/JavaScript/fizzBuzz.js
@@ -1,7 +1,8 @@
 let SampleList = [1,2,3,4,5,6,7,8,9,10];
 
-for (let index = 0; index <= SampleList.length; index++){    
-    if (SampleList[index] % 2 == 0) //if divisible by 2 the fizz else fuzz.
+for (let index = 0; index <= SampleList.length; index++){  
+    //if divisible by 2 the fizz else fuzz.  
+    if (SampleList[index] % 2 == 0) 
         console.log(`${SampleList[index]} goes for a "Fizz".`);
     
     else
